8d4ceec532c1e65a44855a8a10df920cce313221
MaxEichinger43 <mx.eichinger@gmx.de>
2024-01-25 23:11:59 +0100

    added test

--- game.py
+++ game.py
@@ -4,9 +4,26 @@ import os
 strategies_folder = "C:/Users/Der Maxi/Documents/GitHub/Cowtrade/strategies"
 
 STARTING_CAPITAL = 1000
+
+"""
+objects
+"""
+class Strategy:
+    def __init__(self) -> None:
+        self.capital = STARTING_CAPITAL
+        self.invest = 0
+    def get_investment(self):
+        pass
+
+
 strategy_list = []
-# Iteriere durch alle Dateien im Ordner
+
+
+"""
+import
+"""
 i = 0
+# Iteriere durch alle Dateien im Ordner
 for strategy in os.listdir(strategies_folder):
     if strategy.endswith(".py") and strategy != "__init__.py":
         # Extrahiere den Dateinamen ohne Erweiterung
@@ -14,23 +31,19 @@ for strategy in os.listdir(strategies_folder):
 
         try:
             # Versuche die Strategie zu importieren
-            exec(f"import strategies.{strategy_name} as strat{i}")
-            print(f"{strategy_name} erfolgreich als strat{i} importiert")
-            exec(f"strategy_list.append(strat{i})")
-            i += 1
-            
+            exec(f"from strategies.{strategy_name} import *")
+            print(f"{strategy_name} erfolgreich importiert")
+
         except ImportError:
             print(f"{strategy_name} konnte nicht importiert werden")
 
-        exec(f"{strategy_name}_capital = {STARTING_CAPITAL}")
-
-class Strategy:
-    def __init__(self, STARTING_CAPITAL, name) -> None:
-        self.capital = STARTING_CAPITAL
-        self.name = name
-    def get_investment(self):
-        return self.investment()
+        strategy_name = Strategy()
+        strategy_list.append(strategy_name)
+        print("Capital: ",strategy_list[i].capital)
+        strategy_list[i].invest = strategy_list[i].get_investment()
+        print("Invest: ",strategy_list[i].invest)
+        i+=1
 
 
-def get_starting_capital():
+def get_starting_capital(strategy_list):
     pass
